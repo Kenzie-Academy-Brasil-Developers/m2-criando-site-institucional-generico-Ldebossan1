388baf901a973d5b62ac21a9c079b8cd1390cda3
Lucas <debossan200@gmail.com>
2023-05-26 13:25:31 -0300

    quarto commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,18 +3,18 @@ function handleModal() {
     const button = document.querySelector('#showMessage')
     const modalContainer = document.querySelector('#modalContainer')
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         modalContainer.showModal()
 
         closeModal()
     })
 }
 
-function closeModal(){
+function closeModal() {
     const button = document.querySelector('#closeModal')
     const modalContainer = document.querySelector('#modalContainer')
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         modalContainer.close()
     })
 }
